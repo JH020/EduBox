167716e74ac32397a42d043781fa50ae8af55264
Feike Falkena <feike@feikefalkena.nl>
2020-12-04 12:48:32 +0100

    Update standaard bestanden

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Include standaard bestanden -->
+        <?php include('inc/head.php'); ?>
+
+        <!-- Pagina informatie -->
+        <title></title>
+        <meta name="description" content=""/>
+        <link rel="stylesheet" href="">
+
+    </head>
+    <body>
+        <?php include('inc/header.php'); ?>
+    </body>
+</html>
